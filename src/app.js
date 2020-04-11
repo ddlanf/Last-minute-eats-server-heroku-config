241b577ee4c9b3cf90a49114e76fec97e73bb5db
Daniel Laufenberg <danlanfen26@gmail.com>
2020-04-11 00:57:31 +0000

    README added

--- src/app.js
+++ src/app.js
@@ -18,13 +18,11 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 
-app.use(cors())
-/*
 app.use(
   cors({
     origin: CLIENT_ORIGIN,
   })
-)*/
+)
 
 app.use('/api/recipes', recipesRouter)
 app.use('/api/ratings', ratingsRouter)
